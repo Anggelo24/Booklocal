178149b5ea142cd6b99bb7c784eee7c8fed1f223
Anggelo24 <87619610+Anggelo24@users.noreply.github.com>
2025-07-02 09:39:01 -0500

    paginas que faltaban

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import RegistroForm from './components/registroForm';
 import DetallesServicio from './routes/DetallesServicio';
 import PanelProfesional from './routes/PanelProfesional';
 import PerfilProfesional from './routes/PerfilProfesional';
+import Contacto from './routes/Contacto';
+import AcercaDe from './routes/AcercaDe';
+import Privacidad from './routes/Privacidad';
+import Terminos from './routes/Terminos';
 
 function App() {
   return (
@@ -20,6 +24,10 @@ function App() {
           <Route path="/servicio/:id" element={<DetallesServicio />} />
           <Route path="/panelprofesional" element={<PanelProfesional />} />
           <Route path="/perfilprofesional" element={<PerfilProfesional />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/acercade" element={<AcercaDe />} />
+          <Route path="/privacidad" element={<Privacidad />} />
+          <Route path="/terminos" element={<Terminos />} />
         </Routes>
         <Footer />
       </div>
